7fac394bcd453442a16b591dc98c527e31146ce9
Jeremy LAURENT <jeremy-laurent@macbook-pro-de-jeremy.home>
2021-02-01 14:51:58 +0100

    Insertdata

--- database/CreateScheme.sql
+++ database/CreateScheme.sql
@@ -11,12 +11,3 @@ CREATE TABLE public.students
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL
 );
-
-INSERT INTO departments (name) VALUES ('IRC');
-INSERT INTO departments (name) VALUES ('ETI');
-INSERT INTO departments (name) VALUES ('CGP');
-
-INSERT INTO students (department_id, first_name, last_name) VALUES (1, "Eli", "Copter")
-INSERT INTO students (department_id, first_name, last_name) VALUES (2, "alpha", "boby")
-INSERT INTO students (department_id, first_name, last_name) VALUES (3, "beta", "shepard")
-INSERT INTO students (department_id, first_name, last_name) VALUES (4, "charlie", "yolo")
