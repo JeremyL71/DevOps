244dcff728db1a1721294eed755d0db821f14acf
Jeremy LAURENT <jeremy-laurent@macbook-pro-de-jeremy.home>
2021-02-01 19:35:44 +0100

    finish

--- database/InsertData.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO departments (name) VALUES ('IRC');
-INSERT INTO departments (name) VALUES ('ETI');
-INSERT INTO departments (name) VALUES ('CGP');
-
-INSERT INTO students (department_id, first_name, last_name) VALUES (1, "Eli", "Copter")
-INSERT INTO students (department_id, first_name, last_name) VALUES (2, "alpha", "boby")
-INSERT INTO students (department_id, first_name, last_name) VALUES (3, "beta", "shepard")
-INSERT INTO students (department_id, first_name, last_name) VALUES (4, "charlie", "yolo")
